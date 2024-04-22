42ab6dcf662bb32c96107a1ae8c9485e2939ecda
fshhhh <lin.yingw@northeastern.edu>
2024-04-21 20:41:58 -0400

    editor addition

--- src/Kanbas/Courses/Quizzes/Editors/questionmaker.tsx
+++ src/Kanbas/Courses/Quizzes/Editors/questionmaker.tsx
@@ -1,8 +1,14 @@
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import WYSIWYGEditor from "./WYSIWYG";
 import {FaPencil, FaPlus, FaTrashCan} from "react-icons/fa6";
 import {Link, useLocation} from "react-router-dom";
+import * as client from "../client";
+import {setQuiz, setQuizzes} from "../reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {KanbasState} from "../../../store";
+import {useParams} from "react-router";
 
+// @ts-ignore
 function QuestionMaker() {
     const location = useLocation();
     const url = location.pathname;
@@ -24,6 +30,31 @@ function QuestionMaker() {
         //TODO: lol idk
     }
 
+    const { courseId } = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        client.findQuizzesForCourse(courseId)
+            .then((quizzes) =>
+                dispatch(setQuizzes(quizzes))
+            );
+    }, [courseId]);
+
+    const quiz = useSelector((state: KanbasState) =>
+        state.quizzesReducer.quiz);
+
+
+    const handleOptionSelect = (questionId: any, selectedOption: any) => {
+        const updatedQuiz = { ...quiz };
+        const questionIndex = updatedQuiz.questionList.findIndex(
+            (question: { _id: any; }) => question._id === questionId
+        );
+        if (questionIndex !== -1) {
+            updatedQuiz.questionList[questionIndex].answer = selectedOption;
+            setQuiz(updatedQuiz);
+        }
+    };
+
     const handleDeleteOption = (indexToDelete: any) => {
         setOptions(prevOptions => prevOptions.filter((_, index) => index !== indexToDelete));
     };
@@ -33,8 +64,10 @@ function QuestionMaker() {
     }
 
     const handleCancel = () => {
-        {console.log(cancelUrl)}
+       {console.log(cancelUrl)}
+        console.log("questionType:", questionType);
     }
+
 
     return (
         <div style={{marginTop: 50}}>
@@ -44,10 +77,15 @@ function QuestionMaker() {
                            onChange={(e) => setTitle(e.target.value)} style={{width: 200}}/>
                 </div>
                 <div className={"col mb-3"}>
-                    <select defaultValue={questionType} className={"form-select"} style={{width: 200}}>
-                        <option onClick={() => setQuestionType("MCQ")}>Multiple Choice</option>
-                        <option onClick={() => setQuestionType("True/False")}>True/False</option>
-                        <option onClick={() => setQuestionType("Fill in the Blank")}>Fill in the Blank</option>
+                    <select
+                        value={questionType}
+                        onChange={(e) => setQuestionType(e.target.value)}
+                        className="form-select"
+                        style={{ width: 200 }}
+                    >
+                        <option value="MCQ">Multiple Choice</option>
+                        <option value="truefalse">True/False</option>
+                        <option value="Fill in the Blank">Fill in the Blank</option>
                     </select>
                 </div>
                 <div className={"col mb-3 d-flex align-items-center"}>
@@ -62,41 +100,71 @@ function QuestionMaker() {
             <div className={"row mb-3"}>
                 <WYSIWYGEditor value={prompt} onChange={setPrompt}/>
             </div>
-            <h3 style={{marginTop: 50}}>Answers</h3>
 
-            {options.map((option, index) => (
-                <div key={index} className={"flex-fill"}>
-                    <label htmlFor={`option-${index}`}>Options:</label>
-                    <input
-                        type="text"
-                        id={`option-${index}`}
-                        value={option}
-                        onChange={(e) => {
-                            const newOptions = [...options];
-                            newOptions[index] = e.target.value;
-                            setOptions(newOptions);
-                        }}
-                        className="form-control mb-2"
-                        style={{width: 200}}
-                    />
-                    <button onClick={() => setOption(option)}><FaPencil/></button>
-                    <button onClick={() => handleDeleteOption(index)}><FaTrashCan/></button>
-                </div>
-            ))}
+            <h3 style={{marginTop: 100}}>Answers</h3>
 
-            <input type={"text"} value={option} style={{marginTop: 20}}
+
+            {(questionType === 'MCQ' || questionType === 'Fill in the Blank') && (
+                options.map((option : any, index : number) => (
+                    <div key={index} className={"flex-fill"}>
+                        <input
+                            type="radio"
+                            name="question"
+                            value={option}/>
+                        <label htmlFor={`option-${index}`}
+                               style={{marginLeft: 10}}>Option {index + 1}:</label>
+                        <input
+                            type="text"
+                            id={`option-${index}`}
+                            value={option}
+                            onChange={(e) => {
+                                const newOptions = [...options];
+                                newOptions[index] = e.target.value;
+                                setOptions(newOptions);
+                            }}
+                            className="form-control mb-2"
+                            style={{ width: 200 }}
+                        />
+                        <button onClick={() => setOption(option)}>
+                            <FaPencil />
+                        </button>
+                        <button onClick={() => handleDeleteOption(index)}>
+                            <FaTrashCan />
+                        </button>
+                    </div>
+                )))
+            }
+
+            {questionType === 'truefalse' && (
+                    <div className={"flex-fill"}>
+                        <div className={"option"}>
+                            <label><input type="radio" name="answer" value="true" />
+                                <span className={"option-text"}> True</span></label>
+                            <div style={{marginTop: 20}}>
+                            <label><input type="radio" name="answer" value="false" />
+                                <span className={"option-text"}> False</span></label>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+            {(questionType === 'MCQ' || questionType === 'Fill in the Blank') && (
+            <div>
+                <input type={"text"} value={option} style={{marginTop: 20}}
                    onChange={(e) => setOption(e.target.value)}/>
-            <button onClick={handleAddOption}><FaPlus/> Add New Option</button>
-            <button onClick={handleUpdateOption}><FaPencil/> Update Option</button>
+                <button onClick={handleAddOption}><FaPlus/> Add New Option</button>
+                <button onClick={handleUpdateOption}><FaPencil/> Update Option</button>
+            </div>)}
 
             <div className={"btn-toolbar"} style={{marginTop: 20}}>
                 <button className={"btn btn-primary"} style={{backgroundColor: "red"}}
                         onClick={handleUpdate}>Update Question</button>
-                <Link to={cancelUrl} style={{textDecoration: "none"}}>
+                {/*<Link to={cancelUrl} style={{textDecoration: "none"}}>*/}
                     <button className={"btn btn-secondary"}
                             onClick={handleCancel}>Cancel
                     </button>
-                </Link>
+                {/*</Link>*/}
             </div>
         </div>
     )
